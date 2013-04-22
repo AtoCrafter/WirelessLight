48833f3ac35ab033929a48080a6d6ee856738491
AtoCrafter <ato.crafter@gmail.com>
2013-04-23 01:22:45 +0900

    レシピの追加

--- common/ato/wirelesslight/initializer/CommonProxy.java
+++ common/ato/wirelesslight/initializer/CommonProxy.java
@@ -10,12 +10,16 @@ import ato.wirelesslight.item.ItemTransparentizer;
 import ato.wirelesslight.renderer.TileEntityForRender;
 import ato.wirelesslight.tileentity.TileEntityController;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class CommonProxy {
 
     public void load() {
         registerItems();
         GameRegistry.registerTileEntity(TileEntityForRender.class, "dammyForRenderer");
+        registerRecipes();
     }
 
     private void registerItems() {
@@ -49,5 +53,54 @@ public class CommonProxy {
                 "transparentizer"
         );
         GameRegistry.registerTileEntity(TileEntityController.class, "tileentity.controller");
+    }
+
+    private void registerRecipes() {
+        Item itemLightBox = Item.itemsList[WirelessLight.config.blockIDLightBox];
+        Item itemLightPlate = Item.itemsList[WirelessLight.config.blockIDLightPlate];
+        Item itemControllerBox = Item.itemsList[WirelessLight.config.blockIDControllerBox];
+        Item itemController = Item.itemsList[WirelessLight.config.itemIDController+256];
+        Item itemTranparentizer = Item.itemsList[WirelessLight.config.itemIDTransparentizer+256];
+
+        GameRegistry.addRecipe(new ItemStack(itemLightBox), new Object[]{
+                "PPP",
+                "RGR",
+                "PPP",
+                'P', Block.thinGlass,
+                'R', Item.redstone,
+                'G', Item.lightStoneDust,
+        });
+        GameRegistry.addRecipe(new ItemStack(itemLightPlate, 6), new Object[] {
+                "PRP",
+                "GGG",
+                "PPP",
+                'P', Block.thinGlass,
+                'R', Item.redstone,
+                'G', Item.lightStoneDust,
+        });
+        GameRegistry.addRecipe(new ItemStack(itemControllerBox), new Object[] {
+                "ISI",
+                "R R",
+                "ISI",
+                'I', Item.ingotIron,
+                'R', Item.redstone,
+                'S', Item.stick,
+        });
+        GameRegistry.addRecipe(new ItemStack(itemController), new Object[] {
+                "I",
+                "W",
+                "L",
+                'I', Item.ingotIron,
+                'W', itemLightBox,
+                'L', Block.lever,
+        });
+        GameRegistry.addRecipe(new ItemStack(itemTranparentizer), new Object[] {
+                "P",
+                "R",
+                "S",
+                'P', Item.fermentedSpiderEye,
+                'R', Item.redstone,
+                'S', Item.stick,
+        });
     }
 }
